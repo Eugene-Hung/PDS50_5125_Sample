87b07066b2589ac2fe1b8023de25ce73f130bcfd
Eugene Hung <Eugene Hung@Eugene_Hung.ggec.gd>
2020-10-28 10:17:08 +0800

    20201028

--- apps/applications/sink/User/eugene_print.h
+++ apps/applications/sink/User/eugene_print.h
@@ -1,3 +1,36 @@
+/*
+ * 
+ * 　　┏┓　　　┏┓+ +
+ * 　┏┛┻━━━┛┻┓ + +
+ * 　┃　　　　　　　┃ 　
+ * 　┃　　　━　　　┃ ++ + + +
+ *  ████━████ ┃+
+ * 　┃　　　　　　　┃ +
+ * 　┃　　　┻　　　┃
+ * 　┃　　　　　　　┃ + +
+ * 　┗━┓　　　┏━┛
+ * 　　　┃　　　┃　　　　　　　　　　　
+ * 　　　┃　　　┃ + + + +
+ * 　　　┃　　　┃
+ * 　　　┃　　　┃ +  神兽保佑
+ * 　　　┃　　　┃    代码无bug　　
+ * 　　　┃　　　┃　　+　　　　　　　　　
+ * 　　　┃　 　　┗━━━┓ + +
+ * 　　　┃ 　　　　　　　┣┓
+ * 　　　┃ 　　　　　　　┏┛
+ * 　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　┃┫┫　┃┫┫
+ * 　　　　┗┻┛　┗┻┛+ + + +
+ * 
+ * 
+ * @Author: Eugene Hung
+ * @Date: 2020-10-22 11:28:44
+ * @LastEditors: Eugene Hung
+ * @LastEditTime: 2020-10-28 10:16:44
+ * @FilePath: \QCC5125\PDS50_5125_Sample\apps\applications\sink\User\eugene_print.h
+ */
+
+
 #ifndef EUGENE_PRINT_H
 #define EUGENE_PRINT_H
 
